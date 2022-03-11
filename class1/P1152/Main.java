a4c162c8fa40969294c68decf3215eb5c8a9af7c
GitJaeMin <55350092+anjm1020@users.noreply.github.com>
2022-03-11 23:39:27 +0900

    Solved : 1152 - StringTokenizer

---
+++ class1/P1152/Main.java
@@ -0,0 +1,22 @@
+package solved.class1.P1152;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int cnt = 0;
+        while (st.hasMoreTokens()) {
+            st.nextToken();
+            cnt++;
+        }
+
+        bw.write(cnt + "");
+        bw.flush();
+    }
+}
