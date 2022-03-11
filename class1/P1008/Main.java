13de447926f5c4defd16a82fb91349cc1c1702f4
GitJaeMin <55350092+anjm1020@users.noreply.github.com>
2022-03-11 23:39:01 +0900

    Solved : 1008 - Implementation

---
+++ class1/P1008/Main.java
@@ -0,0 +1,20 @@
+package solved.class1.P1008;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int A, B;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        A = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        bw.write(((double)A / B) + "");
+        bw.flush();
+    }
+}
