5c6c0677b83c7a472560adccf6d189a80ea75f12
GitJaeMin <55350092+anjm1020@users.noreply.github.com>
2022-03-18 17:20:55 +0900

    Solved : 10950 - Implementation

---
+++ class1/P10950/Main.java
@@ -0,0 +1,20 @@
+package solved.class1.P10950;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testCase = Integer.parseInt(br.readLine());
+        while (testCase-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            bw.write(A + B + "\n");
+        }
+        bw.flush();
+    }
+}
